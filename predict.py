40f145f535ff2ac418b855f929c2aafcbb445f08
Piyush Kumar <piyush@Piyushs-MacBook-Pro-2.local>
2023-03-22 21:38:02 +0530

    final changes

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
 from cog import BasePredictor, Input, Path
+import os
 
 
 class Predictor(BasePredictor):
@@ -11,12 +12,19 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
+        video_path: Path = Input(description="Input video"),
         scale: float = Input(
             description="Factor to scale image by", ge=0, le=10, default=1.5
         ),
     ) -> Path:
         """Run a single prediction on the model"""
+        generate_vid = "python entry.py --video_name {}".format(video_path)
+        os.system(generate_vid)
+
+        video_name = video_path.split("/")[-1]
+        video_name = video_name.split(".")[0]
+
+        return "./data/test/{}/output.mp4".format(video_name)
         # processed_input = preprocess(image)
         # output = self.model(processed_image, scale)
         # return postprocess(output)
